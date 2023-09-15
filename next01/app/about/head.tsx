f032d5a53558ddf7cadb888345be6971dbe23f58
imrankhakwani <30284761+imrankhakwani@users.noreply.github.com>
2023-09-15 18:44:46 +0500

    new module added

---
+++ next01/app/about/head.tsx
@@ -0,0 +1,10 @@
+export default function head() {
+  return (
+    <>
+    <title>About page</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="description" content="about page of the application" />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+  )
+}
